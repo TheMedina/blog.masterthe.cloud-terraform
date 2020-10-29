9aac5157ad373eaa6f36f5f127414a7018c9d606
TheMedina <medina@skillcappedgames.com>
2020-10-29 17:44:56 -0400

    Adding .tf files for completely compartmentalized Terraform implementation

--- codeBuild.tf
+++ codeBuild.tf
@@ -1,40 +1,34 @@
-#Production CodeBuild deployment
-resource "aws_codebuild_project" "mtcCodeBuild" {
-  name          = "mtcCodeBuild"
-  description   = "MasterTheCloudCodeBuild"
+# AWS CodeBuild Implementation
+resource "aws_codebuild_project" "example" {
+  name          = "test-project"
+  description   = "test_codebuild_project"
   build_timeout = "5"
-  service_role  = "${aws_iam_role.websitebuild.arn}"
+  service_role  = "${aws_iam_role.example.arn}"
 
   artifacts {
     type = "NO_ARTIFACTS"
   }
 
-  environment {
-    type         = "LINUX_CONTAINER"
-    image        = "aws/codebuild/ruby:latest"
-    compute_type = "BUILD_GENERAL1_SMALL"
-
-    environment_variable {
-      name  = "GH_TOKEN"
-      value = "SOME_VALUE1"
-    }
-
-    environment_variable {
-      name  = "SOME_KEY2"
-      value = "SOME_VALUE2"
-      type  = "PARAMETER_STORE"
-    }
+  cache {
+    type     = "S3"
+    location = "${aws_s3_bucket.example.bucket}"
   }
 
-  logs_config {
-    cloudwatch_logs {
-      group_name  = "log-group"
-      stream_name = "log-stream"
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "aws/codebuild/standard:1.0"
+    type                        = "LINUX_CONTAINER"
+    image_pull_credentials_type = "CODEBUILD"
+
+    environment_variable {
+      "name"  = "SOME_KEY1"
+      "value" = "SOME_VALUE1"
     }
 
-    s3_logs {
-      status   = "ENABLED"
-      location = "${aws_s3_bucket.example.id}/build-log"
+    environment_variable {
+      "name"  = "SOME_KEY2"
+      "value" = "SOME_VALUE2"
+      "type"  = "PARAMETER_STORE"
     }
   }
 
@@ -42,29 +36,23 @@ resource "aws_codebuild_project" "mtcCodeBuild" {
     type            = "GITHUB"
     location        = "https://github.com/mitchellh/packer.git"
     git_clone_depth = 1
-
-    git_submodules_config {
-      fetch_submodules = true
-    }
   }
 
-  source_version = "master"
-
   vpc_config {
-    vpc_id = aws_vpc.example.id
+    vpc_id = "vpc-725fca"
 
     subnets = [
-      aws_subnet.example1.id,
-      aws_subnet.example2.id,
+      "subnet-ba35d2e0",
+      "subnet-ab129af1",
     ]
 
     security_group_ids = [
-      aws_security_group.example1.id,
-      aws_security_group.example2.id,
+      "sg-f9f27d91",
+      "sg-e4f48g23",
     ]
   }
 
   tags = {
-    Environment = "Test"
+    "Environment" = "Test"
   }
 }
---
+++ iamRoles.tf
@@ -0,0 +1,19 @@
+#IAM Roles for AWS CodeBuild and CodePipeline
+resource "aws_iam_role" "example" {
+  name = "example"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "codebuild.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
